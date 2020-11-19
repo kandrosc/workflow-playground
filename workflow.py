35c3ea6f785c485450d76a5d1b926f042b129bfa
Kyle Androschuk <kandrosc@ualberta.ca>
2020-11-19 13:51:59 -0700

    success test 5

--- workflow.py
+++ workflow.py
@@ -1,13 +1,10 @@
-
 #!/usr/bin/python3.8
+import sys
 
+def check_even(argv):
+    try: num = int(argv[1])
+    except (IndexError, ValueError) as e: return 1
+    if num%2==1: return 1
+    return 0
 
-
-
-
-
-
-
-def test_fcn(): print(0/1)
-
-if __name__ == "main": test_fcn()
+if __name__=="__main__": sys.exit(check_even(sys.argv))
