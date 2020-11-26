a90336f19457c81fd23c01a5cb9b1e07842120c6
Kyle Androschuk <kandrosc@ualberta.ca>
2020-11-26 11:18:57 -0700

    production server test

--- browser_success.py
+++ browser_success.py
@@ -11,5 +11,4 @@ if __name__ == "__main__":
     url = "http://[2605:fd00:4:1001:f816:3eff:fe53:c1c4]/memoryGame/"
     browser = webdriver.Chrome(FILEPATH+"lin_chromedriver")
     browser.get(url)
-    cookies = pickle.load(open(FILEPATH+"cookies.pkl","rb"))
-    for cookie in cookies: browser.add_cookie(cookie)
+    browser.close()
