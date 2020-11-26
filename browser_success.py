8b6dc80b32d2bb7db359341e5d655a7420834f0a
Kyle Androschuk <kandrosc@ualberta.ca>
2020-11-26 11:33:40 -0700

    production server test

--- browser_success.py
+++ browser_success.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import requests
 
 import pickle
 import os
@@ -16,6 +17,8 @@ FILEPATH = os.path.dirname(os.path.abspath(__file__))+"/"
 
 if __name__ == "__main__":
     url = "http://[2605:fd00:4:1001:f816:3eff:fe53:c1c4]/memoryGame/"
+    r = requests.get(url)
+    print(r.text)
     browser = webdriver.Chrome(FILEPATH+"lin_chromedriver",options=OPTIONS)
     browser.get(url)
     browser.close()
