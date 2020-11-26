ad448dd672b0beac6fbe8a561eff4d697b4ba53c
Kyle Androschuk <kandrosc@ualberta.ca>
2020-11-26 11:20:46 -0700

    production server test

--- browser_success.py
+++ browser_success.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 import pickle
 import os
 
+OPTIONS = Options()
+OPTIONS.add_argument('--headless')
+OPTIONS.add_argument('--no-sandbox')
+OPTIONS.add_argument('--disable-dev-shm-usage')
+OPTIONS.add_argument('--allow-running-insecure-content')
 class ClassNotFoundError(Exception): pass
 
 
@@ -9,6 +16,6 @@ FILEPATH = os.path.dirname(os.path.abspath(__file__))+"/"
 
 if __name__ == "__main__":
     url = "http://[2605:fd00:4:1001:f816:3eff:fe53:c1c4]/memoryGame/"
-    browser = webdriver.Chrome(FILEPATH+"lin_chromedriver")
+    browser = webdriver.Chrome(FILEPATH+"lin_chromedriver",options=OPTIONS)
     browser.get(url)
     browser.close()
